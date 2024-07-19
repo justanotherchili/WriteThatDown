9f9a98aff766ef2a953af7eaaf2dffe4a4bc1c61
justanotherchili <chiyan.li@hotmail.com>
2024-07-19 02:05:45 +0100

    fixed route error

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('notes/', views.NoteListCreate.as_view(), name='note_list'),
-    path('notes/delete/<int:pk/', views.NoteDelete.as_view(), name='delete_note')
+    path('notes/delete/<int:pk>/', views.NoteDelete.as_view(), name='delete_note')
 ]
